c9420f1b7e4ebc28c5aaa008c915260f275a87d4
huulvhe161557 <huulvhe161557@fpt.edu.vn>
2025-06-04 10:37:28 +0700

    update showtime

--- SWP391_Gr3/Pages/Showtimes/Index.cshtml.cs
+++ SWP391_Gr3/Pages/Showtimes/Index.cshtml.cs
@@ -13,6 +13,7 @@ namespace SWP391_Gr3.Pages.Showtimes
         public List<DateOnly> Dates { get; set; } = new();
         public IList<Theater> Theaters { get; set; } = new List<Theater>();
         public IList<Showtime> Showtimes { get; set; } = new List<Showtime>();
+        public List<string> RoomTypes { get; set; } = new();
 
         [BindProperty(SupportsGet = true)]
         public DateOnly? SelectedDate { get; set; }
@@ -20,11 +21,16 @@ namespace SWP391_Gr3.Pages.Showtimes
         public int? TheaterId { get; set; }
         [BindProperty(SupportsGet = true)]
         public string? RoomType { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? MovieId { get; set; }
 
         public async Task OnGetAsync()
         {
             var today = DateOnly.FromDateTime(DateTime.Today);
             Dates = Enumerable.Range(0, 14).Select(i => today.AddDays(i)).ToList();
+
+            if (!SelectedDate.HasValue)
+                SelectedDate = today;
 
             Theaters = await _context.Rooms
                 .Include(r => r.Theater)
@@ -32,11 +38,31 @@ namespace SWP391_Gr3.Pages.Showtimes
                 .Distinct()
                 .ToListAsync();
 
+            if (TheaterId.HasValue)
+            {
+                RoomTypes = await _context.Rooms
+                    .Where(r => r.TheaterId == TheaterId.Value)
+                    .Select(r => r.Code)
+                    .Distinct()
+                    .ToListAsync();
+            }
+            else
+            {
+                RoomTypes = await _context.Rooms
+                    .Select(r => r.Code)
+                    .Distinct()
+                    .ToListAsync();
+            }
+
             var query = _context.Showtimes
-                .Include(s => s.Movie)
                 .Include(s => s.Room).ThenInclude(r => r.Theater)
+                .Include(s => s.Movie)
                 .AsQueryable();
 
+            if (MovieId.HasValue)
+            {
+                query = query.Where(s => s.MovieId == MovieId.Value);
+            }
             if (SelectedDate.HasValue)
             {
                 var date = SelectedDate.Value.ToDateTime(TimeOnly.MinValue).Date;
