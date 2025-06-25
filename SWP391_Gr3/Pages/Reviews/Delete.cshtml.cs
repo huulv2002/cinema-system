30542dc90233441f3197b6381516ce68e83810f1
huulvhe161557 <huulvhe161557@fpt.edu.vn>
2025-06-25 12:43:51 +0700

    add manage review movie

---
+++ SWP391_Gr3/Pages/Reviews/Delete.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SWP391_Gr3.Models;
+
+namespace SWP391_Gr3.Pages.Reviews
+{
+    [Authorize(Roles = "Staff")]
+    public class DeleteModel : PageModel
+    {
+        private readonly SWP391_Gr3.Models.Swp391Context _context;
+
+        public DeleteModel(SWP391_Gr3.Models.Swp391Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public MovieReview MovieReview { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moviereview = await _context.MovieReviews.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (moviereview == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                MovieReview = moviereview;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moviereview = await _context.MovieReviews.FindAsync(id);
+            if (moviereview != null)
+            {
+                MovieReview = moviereview;
+                _context.MovieReviews.Remove(MovieReview);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ SWP391_Gr3/Pages/Reviews/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SWP391_Gr3.Models;
+
+namespace SWP391_Gr3.Pages.Reviews
+{
+    [Authorize(Roles = "Staff")]
+    public class DetailsModel : PageModel
+    {
+        private readonly SWP391_Gr3.Models.Swp391Context _context;
+
+        public DetailsModel(SWP391_Gr3.Models.Swp391Context context)
+        {
+            _context = context;
+        }
+
+        public MovieReview MovieReview { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moviereview = await _context.MovieReviews.FirstOrDefaultAsync(m => m.Id == id);
+            if (moviereview == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                MovieReview = moviereview;
+            }
+            return Page();
+        }
+    }
+}
